dd78709b20e0a9cd4e1bff9dbd0f0812c8fceb94
Super-Cereal <elaineessea@mail.ru>
2020-03-23 21:03:25 +0300

    Альфа версия с PyQt5

---
+++ main.py
@@ -0,0 +1,33 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from time import sleep
+import sys
+import pyautogui
+import random
+
+
+class Interface(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(r'D:\111programming\Python\hack_employers\interface.ui', self)
+        self.run = False
+
+    def update(self):
+        while True:
+            x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+            pyautogui.moveTo(x, y, duration=0.5, tween=pyautogui.easeInOutQuad)
+
+    def keyPressEvent(self, event):
+        if event.key() == 16777238:
+            self.update()
+
+
+if __name__ == "__main__":
+    pyautogui.PAUSE = 0.5
+    pyautogui.FAILSAFE = True
+    WIDTH, HEIGHT = pyautogui.size()
+
+    app = QApplication(sys.argv)
+    inter = Interface()
+    inter.show()
+    app.exec()
